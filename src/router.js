48c4236e912a048bf6b7e5d8647a579a5d82e22b
icodecloud <icodecloud@163.com>
2017-08-17 15:35:04 +0800

    add temp test module ; update router file :  router - subrouter ;

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Router, Route } from 'dva/router';
+import {Router, Route} from 'dva/router';
 import IndexPage from './routes/IndexPage';
-
 import User from "./routes/User.js";
+import Test from "./routes/Test.js";
 
-function RouterConfig({ history }) {
+
+function RouterConfig({history}) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
-      <Route path="/user" component={User} />
+      <Route path="/" component={IndexPage}>
+        <Route path="/user" component={User}/>
+        <Route path="/test" component={Test}/>
+      </Route>
     </Router>
   );
 }
